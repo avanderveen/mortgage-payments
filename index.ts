cc9e63c193db241440f2d50122a29347e72ac12e
Andrew VanderVeen <avanderveen@ea.com>
2023-11-28 19:08:37 +0000

    remove month column for now

--- index.ts
+++ index.ts
@@ -16,7 +16,6 @@ export default function init() {
     document.forms[0].onsubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(document.forms[0]);
-        const start = formData.get("start") ? new Date(formData.get("start") as string) : new Date();
         const payments = mortgagePayments(
             parseFloat(formData.get("principal") as string),
             parseFloat(formData.get("rate") as string),
@@ -30,9 +29,6 @@ export default function init() {
         for (var paymentData of payments) {
             const row = document.createElement("tr");
             addColumn(row, paymentData.paymentId + 1, "text");
-            const date = new Date(start.getFullYear(), start.getMonth(), 1);
-            date.setMonth(start.getMonth() + paymentData.paymentId);
-            addColumn(row, date.toLocaleDateString('en-CA', { year:"numeric", month:"short" }), "text");
             addColumn(row, paymentData.payment);
             addColumn(row, paymentData.interest);
             addColumn(row, paymentData.capital);
