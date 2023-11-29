57d6745ac6ced53e7bdafdc22590f191774c3b4e
Andrew VanderVeen <avanderveen@ea.com>
2023-11-29 00:25:31 +0000

    fix semi-monthly payment schedule

--- index.ts
+++ index.ts
@@ -1,54 +1,45 @@
 import mortgagePayments from 'mp/mortgagePayments.ts';
 
-const CAD = Intl.NumberFormat("en-CA", { style: 'currency', currency: 'CAD' })
+const LOCALE = 'en-CA';
 
-type FrequencyOffset = {
-    unit: string
-    amount: number
+const CAD = Intl.NumberFormat(
+    LOCALE,
+    {
+        style: 'currency',
+        currency: 'CAD'
+    }
+);
+
+const format = (date: Date): string => {
+    return date.toLocaleDateString(
+        LOCALE,
+        {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+        }
+    );
 }
 
 function addColumn(
     row: HTMLTableRowElement,
     content: any,
-    format: "text" | "currency" = "currency"
+    format: 'text' | 'currency' = 'currency'
 ) {
-    const td = document.createElement("td");
-    td.innerHTML = format === "currency" ? CAD.format(content) : content;
+    const td = document.createElement('td');
+    td.innerHTML = format === 'currency' ? CAD.format(content) : content;
     row.appendChild(td);
-}
-
-function paymentDate(
-    start: Date,
-    paymentNumber: number,
-    frequency: FrequencyOffset
-): Date {
-    return moment(start).add(
-        frequency.unit,
-        frequency.amount * paymentNumber
-    ).toDate();
-}
-
-function frequencyOffset(
-    frequency: string
-): FrequencyOffset {
-    switch (frequency) {
-        case "weekly":      return { unit: 'weeks',  amount: 1 };
-        case "biWeekly":    return { unit: 'weeks',  amount: 2 };
-        case "monthly":     return { unit: 'months', amount: 1 };
-        case "semiMonthly": return { unit: 'weeks',  amount: 2 };
-        default: throw new Error(`Not supported: ${frequency}`);
-    }
 }
 
 export default function init() {
     document.forms[0].onsubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(document.forms[0]);
-        const frequency = formData.get("frequency") as string;
-        const startDate = formData.get("start")
-            ? new Date(formData.get("start") as string)
+        const frequency = formData.get('frequency') as string;
+        const startDate = formData.get('start')
+            ? new Date(formData.get('start') as string)
             : new Date();
-        if (frequency === "semiMonthly") {
+        if (frequency === 'semiMonthly') {
             if (Math.abs(startDate.getDate() - 15) < 15) {
                 startDate.setDate(1);
             } else {
@@ -57,34 +48,21 @@ export default function init() {
         }
 
         const payments = mortgagePayments(
-            parseFloat(formData.get("principal") as string),
-            parseFloat(formData.get("rate") as string),
+            parseFloat(formData.get('principal') as string),
+            parseFloat(formData.get('rate') as string),
             frequency,
-            parseFloat(formData.get("term") as string),
-            parseFloat(formData.get("amortization") as string)
+            parseFloat(formData.get('term') as string),
+            parseFloat(formData.get('amortization') as string)
         );
 
-        const tbody = document.getElementsByTagName("tbody")[0];
-        tbody.innerHTML = "";
+        const tbody = document.getElementsByTagName('tbody')[0];
+        tbody.innerHTML = '';
         const offset = frequencyOffset(frequency);
         for (var paymentData of payments) {
-            const row = document.createElement("tr");
-            addColumn(row, paymentData.paymentId + 1, "text");
-
-            const date = paymentDate(
-                startDate,
-                paymentData.paymentId,
-                offset
-            ).toLocaleDateString(
-                'en-CA',
-                {
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric"
-                }
-            );
-            addColumn(row, date, "text");
-
+            const row = document.createElement('tr');
+            const date = offset.paymentDate(startDate, paymentData.paymentId);
+            addColumn(row, paymentData.paymentId + 1, 'text');
+            addColumn(row, format(date), 'text');
             addColumn(row, paymentData.payment);
             addColumn(row, paymentData.interest);
             addColumn(row, paymentData.capital);
@@ -95,4 +73,62 @@ export default function init() {
             tbody.appendChild(row);
         }
     };
+}
+
+interface FrequencyOffset {
+    paymentDate(start: Date, paymentNumber: number): Date;
+}
+
+function frequencyOffset(frequency: string): FrequencyOffset {
+    if (frequency === 'semiMonthly') {
+        return new SemiMonthlyFrequencyOffset();
+    }
+
+    return new SimpleFrequencyOffset(frequency);
+}
+
+class SimpleFrequencyOffset implements FrequencyOffset {
+    private readonly unit: string;
+    private readonly amount: number;
+
+    constructor(frequency: string) {
+        switch (frequency) {
+            case 'weekly':
+                this.unit = 'weeks';
+                this.amount = 1;
+                break;
+            case 'biWeekly':
+                this.unit = 'weeks';
+                this.amount = 2;
+                break;
+            case 'montly':
+                this.unit = 'months';
+                this.amount = 1;
+                break;
+            default:
+                throw new Error(`Not supported: ${frequency}`);
+        }
+    }
+
+    paymentDate(start: Date, paymentNumber: number): Date {
+        return moment(start).add(
+            this.unit,
+            this.amount * paymentNumber
+        ).toDate();
+    }
+}
+
+class SemiMonthlyFrequencyOffset implements FrequencyOffset {
+    paymentDate(start: Date, paymentNumber: number): Date {
+        const date = moment(new Date(start)).add('months', Math.floor(paymentNumber / 2)).toDate();
+        date.setDate(paymentNumber % 2 === 0 ? 1 : 15);
+        return date;
+    }
+}
+
+declare function moment(date: Date): Moment
+
+declare interface Moment {
+    add(unit: string, amount: number): Moment
+    toDate(): Date
 }
