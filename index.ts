561797c592ceaec1bad9c42b1078f21a0dacb684
Andrew VanderVeen <avanderveen@ea.com>
2023-11-29 01:39:43 +0000

    add targeted numbers calculator

--- index.ts
+++ index.ts
@@ -32,15 +32,18 @@ function addColumn(
 }
 
 export default function init() {
-    document.forms[0].onsubmit = (e) => {
+    let payments: PaymentData[] | undefined = undefined;
+
+    const inputs = document.getElementById('inputs') as HTMLFormElement;
+    inputs.onsubmit = (e) => {
         e.preventDefault();
-        const formData = new FormData(document.forms[0]);
+        const formData = new FormData(inputs);
         const frequency = formData.get('frequency') as string;
         const startDate = formData.get('start')
             ? moment(formData.get('start') as string).toDate()
             : new Date();
 
-        const payments = mortgagePayments(
+        payments = mortgagePayments(
             parseFloat(formData.get('principal') as string),
             parseFloat(formData.get('rate') as string),
             frequency,
@@ -51,7 +54,7 @@ export default function init() {
         const tbody = document.getElementsByTagName('tbody')[0];
         tbody.innerHTML = '';
         const offset = frequencyOffset(frequency);
-        for (var paymentData of payments) {
+        for (var paymentData of payments!!) {
             const row = document.createElement('tr');
             const date = offset.paymentDate(startDate, paymentData.paymentId);
             addColumn(row, paymentData.paymentId + 1, 'text');
@@ -65,8 +68,60 @@ export default function init() {
             addColumn(row, paymentData.capitalPaid);
             tbody.appendChild(row);
         }
+
+        updatePop(payments!!);
     };
+    
+    [
+        document.getElementById('pop-type') as HTMLSelectElement,
+        document.getElementById('pop-start') as HTMLInputElement,
+        document.getElementById('pop-end') as HTMLInputElement
+    ].forEach(
+        (input) => input.addEventListener(
+            'change',
+            (e) => {
+                if (payments !== undefined) {
+                    updatePop(payments, input);
+                }
+                return true;
+            }
+        )
+    );
 }
+
+function updatePop(payments: PaymentData[], updated: HTMLElement | undefined = undefined) {
+    const popType = (document.getElementById('pop-type') as HTMLSelectElement).value as ReducerType;
+    const popStart = document.getElementById('pop-start') as HTMLInputElement;
+    const popEnd = document.getElementById('pop-end') as HTMLInputElement;
+    const popAmount = document.getElementById('pop-amount') as HTMLDivElement;
+    if (updated !== popStart) popStart.value = '1';
+    if (updated !== popEnd) popEnd.value = payments.length.toString();
+    popAmount.innerText = CAD.format(
+        payments.slice(
+            parseInt(popStart.value) - 1,
+            parseInt(popEnd.value) - 1
+        ).reduceRight(Reducer[popType], 0)
+    );
+}
+
+type ReducerType = 'total' | 'interest' | 'principal';
+
+type PaymentData = {
+    paymentId: number
+    payment: number
+    interest: number
+    capital: number
+    balance: number
+    amountPaid: number
+    interestPaid: number
+    capitalPaid: number
+};
+
+const Reducer = {
+    total: (acc: number, curr: PaymentData): number => acc + curr.payment,
+    interest: (acc: number, curr: PaymentData): number => acc + curr.interest,
+    principal: (acc: number, curr: PaymentData): number => acc + curr.capital,
+};
 
 interface FrequencyOffset {
     paymentDate(start: Date, paymentNumber: number): Date;
@@ -82,7 +137,7 @@ function frequencyOffset(frequency: string): FrequencyOffset {
 
 class SemiMonthlyFrequencyOffset implements FrequencyOffset {
     paymentDate(start: Date, paymentNumber: number): Date {
-        const date = moment(new Date(start)).add('months', Math.floor(paymentNumber / 2)).toDate();
+        const date = moment(new Date(start)).add(Math.floor(paymentNumber / 2), 'months').toDate();
         date.setDate(paymentNumber % 2 === 0 ? 1 : 15);
         return date;
     }
@@ -113,8 +168,8 @@ class SimpleFrequencyOffset implements FrequencyOffset {
 
     paymentDate(start: Date, paymentNumber: number): Date {
         return moment(start).add(
-            this.unit,
-            this.amount * paymentNumber
+            this.amount * paymentNumber,
+            this.unit
         ).toDate();
     }
 }
@@ -122,6 +177,6 @@ class SimpleFrequencyOffset implements FrequencyOffset {
 declare function moment(date: Date | string): Moment
 
 declare interface Moment {
-    add(unit: string, amount: number): Moment
+    add(amount: number, unit: string): Moment
     toDate(): Date
 }
